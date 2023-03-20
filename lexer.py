3583b499c51fba2f6d25633f01034c40f9414927
coder-newbie404 <74915494+coder-newbie404@users.noreply.github.com>
2023-03-20 21:31:44 +0700

    py import csv/dat
    delete unused cade

--- lexer.py
+++ lexer.py
@@ -1,21 +1,23 @@
 import csv
 
+# import transition_table
 data = list(csv.reader(open('transition_table.dat')))
 
-emptyDict = {}
-
 #map create
-map_state = {}    #map for stete (row) 
-map_key = {}      #map for key (column)
+map_state = {}    # map for stete (row) 
+map_key = {}      # map for key (column)
 
-#insert to map_state
-for i in range(len(data)):              #len(table) = number of row && table format is 1...n as csv not 0.
+# insert to map_state
+for i in range(len(data)):             # len(table) = number of row && table format is 1...n as csv not 0.
     map_state[data[i][0]] = i
 
-#insert to map_key
-for i in range(len(data[0])):          #len(table[1]) = number of column && table format is 1...n as csv not 0,,n
+# insert to map_key
+for i in range(len(data[0])):          # len(table[1]) = number of column && table format is 1...n as csv not 0,,n
     map_key[data[0][i]] = i
 
-#get value at state 21 key "C"    
+# get value at state 0 key "+"    
 print( data[ map_state['0']][map_key['+']] )
-#The .get() method returns an Option<&V> 
+
+# aAccess data guide: data[ map_state['0']][map_key['+']] 
+# State is string (very stupid but can ez change to int)
+# key is string
